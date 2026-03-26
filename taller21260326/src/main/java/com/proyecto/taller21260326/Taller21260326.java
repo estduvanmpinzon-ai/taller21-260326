a0a483ef02c424f81d9bc78d5bf691907ab7098a
Duvan <est,duvanm.pinzon@unimilitar.edu.co>
2026-03-26 10:04:26 -0500

    se inicializo el proyecto y creo la clase lista enlazada

---
+++ taller21260326/src/main/java/com/proyecto/taller21260326/Taller21260326.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.proyecto.taller21260326;
+
+/**
+ *
+ * @author Admin
+ */
+public class Taller21260326 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
