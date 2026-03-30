01b1aaf3c4095ff840421ffcd8c947b485ac6d74
duvan <duvan@estduvanmpinzon-ai>
2026-03-30 13:18:45 -0500

    se empezaron a unificar todas las clases

---
+++ taler21260326/src/main/java/NumeroFaltante.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class NumeroFaltante {
+
+    static class Nodo {
+        int dato;
+        Nodo siguiente;
+
+        public Nodo(int dato) {
+            this.dato = dato;
+            this.siguiente = null;
+        }
+    }
+
+    
